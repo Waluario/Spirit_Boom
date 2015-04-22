7391d45d260d160b1e5bf63bfbcad22b91c2c42e
RumpNissen89 <gustav_hassler@hotmail.com>
2015-04-22 10:49:33 +0200

    Main-, Pause- and Battle Menu (Basic)

--- Spirit_Boom/Assets/hidden.cs
+++ Spirit_Boom/Assets/hidden.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class hidden : MonoBehaviour {
 
-
+	public GameObject canvas;
 
 	public bool optionsOn = false;
 
 	public void turnOnOptions(){
 		
 		if (optionsOn == false) {
-			
-			
+
+			optionsOn = true;
+			canvas.SetActive(false);		} 
+		else {
+
+			optionsOn = false;
+			canvas.SetActive(true);
 		}
 	}
 
